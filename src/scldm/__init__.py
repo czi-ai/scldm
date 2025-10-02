32e4a3c28cdee791f9f4ba2962e423d4c3a39795
giovp <gpalla@chanzuckerberg.com>
2025-10-02 09:08:46 -0700

    update

--- src/scldm/__init__.py
+++ src/scldm/__init__.py
@@ -1,7 +1,22 @@
 from importlib.metadata import version
 
+from .models import (
+    build_diffusion_model,
+    build_model_from_config,
+    build_scvi_vae,
+    build_transformer_vae,
+    build_transport,
+)
 from .vae import ScviVAE, TransformerVAE
 
-__all__ = ["ScviVAE", "TransformerVAE"]
+__all__ = [
+    "ScviVAE",
+    "TransformerVAE",
+    "build_transformer_vae",
+    "build_scvi_vae",
+    "build_diffusion_model",
+    "build_transport",
+    "build_model_from_config",
+]
 
 __version__ = version("scldm")
